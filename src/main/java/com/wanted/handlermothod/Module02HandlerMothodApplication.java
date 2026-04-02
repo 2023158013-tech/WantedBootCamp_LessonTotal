1712aa3b055f9eecb54cb0e6d86e05547110fb4b
2023158013-tech <2023158013@g.eulji.ac.kr>
2026-04-02 15:45:19 +0900

    Initial commit

---
+++ src/main/java/com/wanted/handlermothod/Module02HandlerMothodApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.handlermothod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module02HandlerMothodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module02HandlerMothodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wanted/handlermothod/Module02HandlerMothodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.handlermothod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module02HandlerMothodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
