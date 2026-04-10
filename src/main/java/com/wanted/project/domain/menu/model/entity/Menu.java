3d94a7e7a91d36e19dbcb017fba0e3c892752df3
2023158013-tech <2023158013@g.eulji.ac.kr>
2026-04-10 13:15:39 +0900

    4.10

--- src/main/java/com/wanted/project/domain/menu/model/entity/Menu.java
+++ src/main/java/com/wanted/project/domain/menu/model/entity/Menu.java
@@ -1,17 +1,15 @@
 package com.wanted.project.domain.menu.model.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-import org.springframework.stereotype.Component;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @ToString
 @Entity
+/* 2. @Builder 어노테이션을 사용한 update 기능*/
+//@Builder(toBuilder = true)
 @Table(name = "tbl_menu")
 public class Menu {
 
@@ -31,4 +29,23 @@ public class Menu {
 
     @Column(name = "orderable_status")
     private String orderableStatus;
+
+    /* 수정 방법 3가지
+     *  1. setter 사용해서 update -> setter 사용은 지양한다. (변경된 객체 추적이 힘듦)*/
+//    public void setMenuName(String menuName) {
+//        this.menuName = menuName;
+//    }
+
+    /* 2. @Builder 어노테이션을 사용한 update 기능*/
+
+    /* 3. Entity 내부에 직접 Builder 패턴을 구현*/
+    public Menu changeMenuName(String newMenuName) {
+        this.menuName = newMenuName;
+        return this;
+    }
+
+    public Menu builder() {
+        return new Menu(menuCode, menuName, menuPrice, categoryCode, orderableStatus);
+    }
+
 }
